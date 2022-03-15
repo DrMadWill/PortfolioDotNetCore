27666650dadda0f0d88d0d66a576580b224cc5e5
DrMadWill <nofelsalahov9@gmail.com>
2022-03-15 14:12:39 +0400

    main:Login and Register View + Method Complate
    SiginIn Controller Add (Resiter, Login and LogOut) method + View Complate

--- Portfolio_Back_End/ParfolioWebSiteView/Controllers/SigninController.cs
+++ Portfolio_Back_End/ParfolioWebSiteView/Controllers/SigninController.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using ParfolioWebSiteView.ViewModels;
 
 namespace ParfolioWebSiteView.Controllers
 {
@@ -30,14 +31,116 @@ namespace ParfolioWebSiteView.Controllers
             signInManager = _signInManager;
         }
 
-        public async Task<IActionResult> Login()
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            ViewBag.IsShowing = false;
+            SignInVM signIn = new SignInVM();
+
+            return View(signIn);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(SignInVM signIn)
+        {
+            ViewBag.IsShowing = false;
+            if (!ModelState.IsValid) return View(signIn);
+
+            var user = await userManager.FindByEmailAsync(signIn.Email);
+            if(user==null)
+            {
+                ViewBag.IsShowing = true;
+                ModelState.AddModelError("Email", "Email is Wrong!");
+                return View(signIn);
+            }
+
+            var resault = await signInManager.PasswordSignInAsync(user, signIn.Password, signIn.RememberMe, false);
+            if (resault.Succeeded)
+                return Redirect("/Admin/Account/List");
+
+            ViewBag.IsShowing = true;
+            ModelState.AddModelError("Password", "Password Wrong!");
+            return View(signIn);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            ViewBag.IsShowing = false;
+            SignUpVM sign = new SignUpVM();
+            return View(sign);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(SignUpVM sign)
         {
 
+            ViewBag.IsShowing = false;
+            if (!ModelState.IsValid) return View(sign);
 
-            return View();
+            // Check User Name Unique 
+            var userTest = await userManager.FindByNameAsync(sign.UserName);
+            if (userTest != null)
+            {
+                ViewBag.IsShowing = true;
+                ModelState.AddModelError("User", "User Name Already Used.");
+                return View(sign);
+            }
+
+
+            // Ckeck Password Usable
+            var passwordValidator = new PasswordValidator<User>();
+            var result = await passwordValidator.ValidateAsync(userManager, null, sign.Password);
+            if (!result.Succeeded)
+            {
+                ViewBag.IsShowing = true;
+                foreach (var item in result.Errors)
+                    ModelState.AddModelError("User", item.Description);
+                return View(sign);
+            }
+
+
+            // Check Email Unique And Anouther Opertion
+            if (userManager.FindByEmailAsync(sign.Email).Result == null)
+            {
+                User user = new User
+                {
+                    Image = "profil.png",
+                    Email = sign.Email,
+                    FirstName = sign.FirstName,
+                    LastName = sign.LastName,
+                    UserName = sign.UserName,
+                    Gender = sign.Gender
+                };
+                IdentityResult resault = await userManager.CreateAsync(user, sign.Password);
+                if (resault.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user,role:"User");
+                    await dbContext.SaveChangesAsync();
+                    await signInManager.SignInAsync(user, true);
+                    return Redirect("/Admin/Account/List");
+                }
+            }
+            else
+            {
+                ViewBag.IsShowing = true;
+                ModelState.AddModelError("Email Error", "This Email Already Using");
+                return View(sign);
+
+            }
+            ViewBag.IsShowing = true;
+            ModelState.AddModelError("Error", "Sorry. Account Not Created.");
+            return View(sign);
         }
 
 
-
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return Redirect("/Admin/Account/Login");
+        }
     }
 }
---
+++ Portfolio_Back_End/ParfolioWebSiteView/ViewModels/SignInVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParfolioWebSiteView.ViewModels
+{
+    public class SignInVM
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
