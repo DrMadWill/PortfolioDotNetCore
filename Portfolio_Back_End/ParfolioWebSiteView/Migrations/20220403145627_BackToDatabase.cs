344059331f7c137ac73ace250ee2acd6656cd753
DrMadWill <nofelsalahov9@gmail.com>
2022-04-03 19:42:10 +0400

    Task Basket Complate

---
+++ Portfolio_Back_End/ParfolioWebSiteView/Migrations/20220403145627_BackToDatabase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParfolioWebSiteView.Migrations
+{
+    public partial class BackToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seals");
+
+            migrationBuilder.DropTable(
+                name: "Prodacts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prodacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prodacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Count = table.Column<float>(type: "real", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    ProdactId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seals_Prodacts_ProdactId",
+                        column: x => x.ProdactId,
+                        principalTable: "Prodacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Seals_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seals_ProdactId",
+                table: "Seals",
+                column: "ProdactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seals_UserId",
+                table: "Seals",
+                column: "UserId");
+        }
+    }
+}
